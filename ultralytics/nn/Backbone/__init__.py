230338b42a30e49c4b440605defa5cbb3ccae86c
白鹿 <59153249+wujie-max@users.noreply.github.com>
2025-07-04 16:57:43 +0800

    Delete ultralytics directory

--- ultralytics/nn/Backbone/__init__.py
+++
@@ -1,7 +0,0 @@
-from .GhostNetv1 import Ghostnetv1
-from .MobileNetv1 import MobileNetV1
-from .starnet import starnet_s1, starnet_s2, starnet_s3, starnet_s4
-from .mobilenetv4 import MobileNetV4ConvSmall
-
-# layers
-from .layers import ghostlayer
